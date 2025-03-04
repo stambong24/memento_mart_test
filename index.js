904e42f9eb5841d67b37224877b867f6d208e59b
stambong24 <tambong.sophia24@gmail.com>
2025-03-04 14:29:33 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$("#main-content").hide()
+
+$("#splash-screen").click(function(){
+
+    $("#splash-screen").fadeOut(1500);
+    $("#splash-screen").addClass("active")
+    $("#main-content").fadeIn(6000);
+    $("#receipt").addClass("active-2")
+    $("#about-button").addClass("button-fade")
+    $("#browse-button").addClass("button-fade")
+
+})
+
---
+++ javascript/aisleselection.js
@@ -0,0 +1,16 @@
+/*$("#sweet").hover(function(){
+    $("#pocky").toggleClass("active-red-left-1")
+    $("#rabbit").toggleClass("active-red-left-2")
+    $("#panda").toggleClass("active-red-left-3")
+    $("#sachima").toggleClass("active-red-right-1")
+    $("#ramune").toggleClass("active-red-right-2")
+    $("#yakult").toggleClass("active-red-right-3")
+});
+
+$("#salty").hover(function(){
+    $("#shrimp").toggleClass("active-blue-left-1")
+   
+});
+
+*/
+
